b443d9342668d7c815b66341493045ba6eaab4b8
lian-c <lian.chan@telus.com>
2022-11-24 21:46:55 -0800

    added response status code if there's any other issue it would provide the error

--- iss.js
+++ iss.js
@@ -6,20 +6,24 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const request = require('request');
- const fetchMyIP = function(callback) { 
+const request = require('request');
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if (error){
-      return callback(error,null); 
+    if (error) {
+      return callback(error,null);
     }
-    // console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
    
-      const nonstring = JSON.parse(body)
-      const result = nonstring.ip
-      return callback(null, result); //remember to use null as it takes two parameters 
+    const nonstring = JSON.parse(body); //give the object
+    const result = nonstring.ip; // takes just the ip key to return the value as a string
+    return callback(null, result); //remember to use null as it takes two parameters
     
   });
-}
+};
 
 module.exports = { fetchMyIP };
