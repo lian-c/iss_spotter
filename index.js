d08a986250e0fc3c4fb2b15bf6f227a82cf6f09b
lian-c <lian.chan@telus.com>
2022-11-24 22:59:37 -0800

    commented index.js as tasked

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchCoordsByIP("50.98.71.238",(error, data) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchCoordsByIP("50.98.71.238",(error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned data:' , data);
-});
+//   console.log('It worked! Returned data:' , data);
+// });
